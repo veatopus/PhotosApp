866461f1854e6ec3da7522198b9e59197d9bc2f2
veatopus <veatopus@gmail.com>
2022-04-20 22:29:45 +0600

    added Prefs calss for work with SP

--- core/src/main/java/kg/ruslan/core/di/CoreModule.kt
+++ core/src/main/java/kg/ruslan/core/di/CoreModule.kt
@@ -1,9 +1,19 @@
 package kg.ruslan.core.di
 
 import kg.ruslan.core.halpers.Utils
+import kg.ruslan.core.sharedpreference.GreetingFragmentOpened
+import kg.ruslan.core.sharedpreference.Prefs
 import org.koin.android.ext.koin.androidApplication
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val coreModule = module {
+    // utils
     factory { Utils(androidApplication()) }
+
+    //shared preferences
+    factory { Prefs(androidContext()) }
+    factory { get<Prefs>().getGreetingSharedPreferences() }
+
+    factory { GreetingFragmentOpened(get()) }
 }
