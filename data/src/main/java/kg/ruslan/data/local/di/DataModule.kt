4a86b1559250eae67bcafc026e3c54ec5c0c504d
veatopus <veatopus@gmail.com>
2022-04-20 17:08:46 +0600

    added photos api to koin module

--- data/src/main/java/kg/ruslan/data/local/di/DataModule.kt
+++ data/src/main/java/kg/ruslan/data/local/di/DataModule.kt
@@ -1,9 +1,13 @@
 package kg.ruslan.data.local.di
 
 import kg.ruslan.core.apis.repositories.CandidateInfoApi
+import kg.ruslan.core.apis.repositories.LocalPhotosApi
 import kg.ruslan.data.local.repositories.impl.CandidateInfoApiImpl
+import kg.ruslan.data.local.repositories.impl.LocalPhotosApiImpl
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val dataModule = module {
     factory<CandidateInfoApi> { CandidateInfoApiImpl(utils = get()) }
+    factory<LocalPhotosApi> { LocalPhotosApiImpl(context = androidContext()) }
 }
